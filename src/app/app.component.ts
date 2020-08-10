f8819141d7a2e096077a4c1b20f909fbd555fd4e
Oscar Daniel Perez <odprz.dev@gmail.com>
2020-08-09 21:09:43 -0500

    chore: se modifican urls y corrigen detalles gramaticaless

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SeoTagsService } from './services/seo-tags.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'odprz-page';
+  constructor(private seoTagsSrvice: SeoTagsService){
+    this.seoTagsSrvice.setTitleAndTags();
+  }
 }
